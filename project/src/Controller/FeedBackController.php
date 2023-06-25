968ff51d56460fa9554c81d4d4c3b1c4ad9a5e40
novapc74 <novapc@ukr.net>
2023-06-25 14:41:12 +0300

    add feedback controller & feddbackFormType

--- project/src/Controller/FeedBackController.php
+++ project/src/Controller/FeedBackController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\FeedBack;
+use App\Form\FeedBackFormType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FeedBackController extends AbstractController
 {
-    #[Route('/feed/back', name: 'app_feed_back')]
-    public function index(): Response
+    #[Route('/feed/back', name: 'app_feed_back', methods: ['GET', 'POST'])]
+    public function index(Request $request, ManagerRegistry $managerRegistry): Response
     {
-        return $this->render('feed_back/index.html.twig', [
-            'controller_name' => 'FeedBackController',
+        $feedBack = new FeedBack();
+        $form = $this->createForm(FeedBackFormType::class, $feedBack);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $feedBack = $form->getData();
+
+            $em = $managerRegistry->getManager();
+            $em->persist($feedBack);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home_page');
+        }
+
+        return $this->render('feed_back/feed_back_form.html.twig', [
+            'feedBackForm' => $form->createView(),
         ]);
     }
 }
--- project/src/Entity/Trait/TitleDescriptionTrait.php
+++ project/src/Entity/Trait/TitleDescriptionTrait.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Entity\Trait;
+
 use Doctrine\ORM\Mapping as ORM;
 
 trait TitleDescriptionTrait {
