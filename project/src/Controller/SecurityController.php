88fa5f989040bdc0bc44fd6ce49d5a68338a5606
novapc74 <novapc@ukr.net>
2023-07-01 18:52:53 +0300

    add redirect to login, if user

--- project/src/Controller/SecurityController.php
+++ project/src/Controller/SecurityController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class SecurityController extends AbstractController
 {
+    use TargetPathTrait;
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-         if ($this->getUser()) {
-             return $this->redirectToRoute('target_path');
-         }
+        /**@var User $user */
+        $user = $this->getUser();
+        if ($user && in_array("ROLE_ADMIN", $user->getRoles())) {
+            return $this->redirectToRoute('admin');
+        }
 
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
@@ -25,6 +31,5 @@ class SecurityController extends AbstractController
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
