ff846e65337236d327f789b1f147858d22f7fa1d
kosatos <kosatosfront@ya.ru>
2023-07-05 02:05:12 +0300

    add header + hero-section + footer

--- project/src/Controller/HomePageController.php
+++ project/src/Controller/HomePageController.php
@@ -11,7 +11,7 @@ class HomePageController extends AbstractController
     #[Route('/', name: 'app_home_page')]
     public function index(): Response
     {
-        return $this->render('home_page/index.html.twig', [
+        return $this->render('pages/home.html.twig', [
         ]);
     }
 }
