b97319dc4ca909868bba2b8fd25963ac20197bd1
Вячеслав Макаров <makarov@positron-it.ru>
2023-07-06 17:55:05 +0300

    add explode address

--- project/src/Entity/Contact.php
+++ project/src/Entity/Contact.php
@@ -11,96 +11,101 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: ContactRepository::class)]
 class Contact
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+	#[ORM\Id]
+	#[ORM\GeneratedValue]
+	#[ORM\Column]
+	private ?int $id = null;
 
-    #[ORM\Column(length: 20)]
-    private ?string $phone = null;
+	#[ORM\Column(length: 20)]
+	private ?string $phone = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $email = null;
+	#[ORM\Column(length: 255)]
+	private ?string $email = null;
 
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $address = null;
+	#[ORM\Column(type: Types::TEXT)]
+	private ?string $address = null;
 
-    #[ORM\OneToMany(mappedBy: 'contact', targetEntity: Social::class)]
-    private Collection $socials;
+	#[ORM\OneToMany(mappedBy: 'contact', targetEntity: Social::class)]
+	private Collection $socials;
 
-    public function __construct()
-    {
-        $this->socials = new ArrayCollection();
-    }
+	public function __construct()
+	{
+		$this->socials = new ArrayCollection();
+	}
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
 
-    public function getPhone(): ?string
-    {
-        return $this->phone;
-    }
+	public function getPhone(): ?string
+	{
+		return $this->phone;
+	}
 
-    public function setPhone(string $phone): static
-    {
-        $this->phone = $phone;
+	public function setPhone(string $phone): static
+	{
+		$this->phone = $phone;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
+	public function getEmail(): ?string
+	{
+		return $this->email;
+	}
 
-    public function setEmail(string $email): static
-    {
-        $this->email = $email;
+	public function setEmail(string $email): static
+	{
+		$this->email = $email;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getAddress(): ?string
-    {
-        return $this->address;
-    }
+	public function getAddress(): ?string
+	{
+		return $this->address;
+	}
 
-    public function setAddress(string $address): static
-    {
-        $this->address = $address;
+	public function setAddress(string $address): static
+	{
+		$this->address = $address;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @return Collection<int, Social>
-     */
-    public function getSocials(): Collection
-    {
-        return $this->socials;
-    }
+	/**
+	 * @return Collection<int, Social>
+	 */
+	public function getSocials(): Collection
+	{
+		return $this->socials;
+	}
 
-    public function addSocial(Social $social): static
-    {
-        if (!$this->socials->contains($social)) {
-            $this->socials->add($social);
-            $social->setContact($this);
-        }
+	public function addSocial(Social $social): static
+	{
+		if (!$this->socials->contains($social)) {
+			$this->socials->add($social);
+			$social->setContact($this);
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function removeSocial(Social $social): static
-    {
-        if ($this->socials->removeElement($social)) {
-            // set the owning side to null (unless already changed)
-            if ($social->getContact() === $this) {
-                $social->setContact(null);
-            }
-        }
+	public function removeSocial(Social $social): static
+	{
+		if ($this->socials->removeElement($social)) {
+			// set the owning side to null (unless already changed)
+			if ($social->getContact() === $this) {
+				$social->setContact(null);
+			}
+		}
 
-        return $this;
-    }
+		return $this;
+	}
+
+	public function explodeAddress(): array
+	{
+		return explode("\r\n\r\n", $this->address);
+	}
 }
