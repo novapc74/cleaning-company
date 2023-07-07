abd4318bd2fb15dc61cf27524ba12a5731d33d51
Вячеслав Макаров <makarov@positron-it.ru>
2023-07-07 18:12:24 +0300

    add gallery to home page

--- project/src/Controller/HomePageController.php
+++ project/src/Controller/HomePageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GalleryRepository;
 use App\Repository\PageSectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,14 +11,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(PageSectionRepository $pageSectionRepository): Response
+    public function index(PageSectionRepository $pageSectionRepository, GalleryRepository $galleryRepository): Response
     {
         return $this->render('pages/home.html.twig', [
 	        'about_collection' => $pageSectionRepository->findBy(['type' => 5]),
 	        'how_it_works_collection' => $pageSectionRepository->findBy(['type' => 1]),
 	        'question_answer_collection' => $pageSectionRepository->findBy(['type' => 2]),
 	        'services_collection' => $pageSectionRepository->findBy(['type' => 3]),
-	        'advantages_collection' => $pageSectionRepository->findBy(['type' => 4])
+	        'advantages_collection' => $pageSectionRepository->findBy(['type' => 4]),
+	        'gallery_collection' => $galleryRepository->findBy(['type' => 1]),
         ]);
     }
 }
