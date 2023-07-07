8ef65ef733cdd800178d7b4fc17d37f6768cb762
kosatos <kosatosfront@ya.ru>
2023-07-08 01:50:59 +0300

    cases swiper + reviews swiper

--- project/assets/app/src/js/base/swipers.js
+++ project/assets/app/src/js/base/swipers.js
@@ -18,6 +18,53 @@ export default function swipers() {
             nextEl: '.gallery-slider__btn_next',
             prevEl: '.gallery-slider__btn_prev',
         },
+    })
 
+    const casesSwiper = new Swiper('.cases-swiper', {
+        slidesPerView: 1,
+        spaceBetween: 15,
+        loop: false,
+        speed: 500,
+        navigation: {
+            nextEl: '.cases-swiper__btn_next',
+            prevEl: '.cases-swiper__btn_prev',
+        },
+        breakpoint: {
+            768: {
+                slidesPerView: 2,
+            },
+            1280: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            1600: {
+                slidesPerView: 2,
+                spaceBetween: 60
+            },
+        }
+    })
+
+    const reviewsSwiper = new Swiper('.reviews-swiper', {
+        slidesPerView: 1,
+        spaceBetween: 15,
+        loop: false,
+        speed: 500,
+        navigation: {
+            nextEl: '.reviews-swiper__btn_next',
+            prevEl: '.reviews-swiper__btn_prev',
+        },
+        breakpoint: {
+            768: {
+                slidesPerView: 2,
+            },
+            1280: {
+                slidesPerView: 3,
+                spaceBetween: 50
+            },
+            1600: {
+                slidesPerView: 3,
+                spaceBetween: 60
+            },
+        }
     })
 }
