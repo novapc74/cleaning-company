7c93753fa4cb2cca0d9c3d6a205c94e586e58550
novapc74 <novapc@ukr.net>
2023-07-09 18:04:31 +0300

    add to methods for feedback

--- project/src/Controller/FeedbackController.php
+++ project/src/Controller/FeedbackController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Feedback;
 use App\Form\Admin\FeedbackFormType;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,29 +13,62 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FeedbackController extends AbstractController
 {
-	#[Route('/feedback', name: 'app_feedback', methods: ['GET', 'POST'])]
-	public function index(Request $request, ManagerRegistry $managerRegistry): Response
-	{
+    public function __construct(private readonly ManagerRegistry $managerRegistry)
+    {
+    }
+
+//    #[Route('/feedback', name: 'app_feedback', methods: ['GET', 'POST'])]
+//	public function index(Request $request, ManagerRegistry $managerRegistry): Response
+//	{
 //		if (!$request->isXmlHttpRequest()) {
 //			return $this->redirectToRoute('app_home_page');
 //		}
+//
+//		$feedBack = new Feedback();
+//		$form     = $this->createForm(FeedbackFormType::class, $feedBack);
+//
+//		$form->handleRequest($request);
+//		if ($form->isSubmitted() && $form->isValid()) {
+//			$feedBack = $form->getData();
+//
+//			$em = $managerRegistry->getManager();
+//			$em->persist($feedBack);
+//			$em->flush();
+//
+//			return new Response(true, 201);
+//		}
+//
+//		return $this->render("feedback/form.html.twig", [
+//			'feedbackForm' => $form->createView(),
+//		]);
+//	}
 
-		$feedBack = new Feedback();
-		$form     = $this->createForm(FeedbackFormType::class, $feedBack);
+    #[Route('/feedback_header', name: 'app_feedback_header', methods: ['POST'])]
+    public function setHeaderFeedback(Request $request): Response
+    {
+        $this->setFeedbackData($request);
 
-		$form->handleRequest($request);
-		if ($form->isSubmitted() && $form->isValid()) {
-			$feedBack = $form->getData();
+        return new Response(true, 201);
+    }
 
-			$em = $managerRegistry->getManager();
-			$em->persist($feedBack);
-			$em->flush();
+    #[Route('/feedback_footer', name: 'app_feedback_footer', methods: ['POST'])]
+    public function setFooterFeedback(Request $request): Response
+    {
+        $this->setFeedbackData($request);
 
-			return new Response(true, 201);
-		}
+        return new Response(true, 201);
+    }
 
-		return $this->render("feedback/form.html.twig", [
-			'feedbackForm' => $form->createView(),
-		]);
-	}
+    private function setFeedbackData($request): void
+    {
+        $feedback = new Feedback();
+
+        $feedback->setName($request->request->get('name'));
+        $feedback->setEmail($request->request->get('email'));
+        $feedback->setPhone($request->request->get('phone'));
+
+        $em = $this->managerRegistry->getManager();
+        $em->persist($feedback);
+        $em->flush();
+    }
 }
